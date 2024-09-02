15c948155377e6aba7ac9fdd0bbb7eee5b287bd0
SayarB <sayarbhattacharyya03@gmail.com>
2024-09-02 18:06:26 +0530

    added route to enable mfa and register face

--- golang-server/pkg/routes/routes.go
+++ golang-server/pkg/routes/routes.go
@@ -1,7 +1,14 @@
 package routes
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/mfa-face-recog/pkg/config"
@@ -13,12 +20,12 @@ type UserRegisterRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
-
 type User struct {
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+	MFA      bool   `json:"mfa"`
 }
 
 type UserLoginRequest struct {
@@ -73,4 +80,78 @@ func RegisterRoutes(app *fiber.App) {
 		}
 		return c.Status(fiber.StatusOK).JSON(&fiber.Map{"success": "true"})
 	})
+	app.Post("/api/v1/mfa/face/register", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.FormValue("user_id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Invalid user ID")
+		}
+		faceImage, err := c.FormFile("face_image")
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("User ID and face image are required")
+		}
+		user := User{
+			ID: id,
+		}
+		fmt.Println(id)
+		err = config.DB.Get(&user, "SELECT * FROM users WHERE id = $1", id)
+		if err != nil {
+			fmt.Print(err)
+			return c.Status(fiber.StatusBadRequest).SendString("User not found")
+		}
+		if user.MFA {
+			return c.Status(fiber.StatusBadRequest).SendString("User already has MFA enabled")
+		}
+
+		fileRead, err := faceImage.Open()
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Error opening face image")
+		}
+		err = SendImageToFaceRecognitionService(fileRead, strconv.Itoa(id))
+		if err != nil {
+			log.Println(err)
+			return c.Status(fiber.StatusBadRequest).SendString("Error sending image to face recognition service")
+		}
+
+		config.DB.MustExec(`UPDATE users SET mfa = true WHERE id = $1`, id)
+		return c.Status(fiber.StatusOK).JSON(&fiber.Map{"success": "true"})
+
+	})
+}
+
+func SendImageToFaceRecognitionService(fileRead io.Reader, name string) error {
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	fw, err := w.CreateFormFile("image", "face.jpg")
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(fw, fileRead)
+	if err != nil {
+		return err
+	}
+	nw, err := w.CreateFormField("name")
+	if err != nil {
+		return err
+	}
+	_, err = io.WriteString(nw, name)
+	if err != nil {
+		return err
+	}
+	w.Close()
+	req, err := http.NewRequest("POST", "http://localhost:5000/api/v1/register", &b)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return errors.New("face recognition service returned non-200 status code")
+	}
+	fmt.Println(resp.StatusCode)
+	return nil
 }
